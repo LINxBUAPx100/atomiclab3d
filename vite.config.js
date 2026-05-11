63966b6c15f1f096f3b058a23ec5f5ef138b797d
LINTAREAS <ts202534004@alm.buap.mx>
2026-05-10 18:57:41 -0600

    los cambios

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/atomiclabs3d/', // <-- Tienes que agregar esta línea exactamente así
-  // ...
-})
+  plugins: [react()],
+  base: '/atomiclabs3d/',
 })
