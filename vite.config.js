2a43cd55de42e026cfd3c33a4c6f93da040e8d8b
LINcer0 <ts202534004@alm.buap.mx>
2026-05-10 18:46:49 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // Esta línea es la más importante para que GitHub Pages 
-  // encuentre tus archivos en la subcarpeta del repositorio.
-  base: '/atomiclabs3d/',
+  base: '/atomiclabs3d/', // <-- Tienes que agregar esta línea exactamente así
+  // ...
+})
 })
