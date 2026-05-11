7afda944fe85542fe66e4f08d77a080422aefdce
LINTAREAS <ts202534004@alm.buap.mx>
2026-05-11 01:55:20 -0600

    podria ser

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app.jsx' 
-import './index.css' // ¡Esta línea es la que conecta todo el diseño!
+import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
